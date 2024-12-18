4fba289a9923d637196221f4c09cbabb7650edf6
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2024-12-18 18:31:31 -0300

    feat: :sparkles: bootstrap file

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,4 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 //bootstrap
-(async () => await (await NestFactory.create(AppModule)).listen(3000))()
+(async () => await (await NestFactory.create(AppModule)).listen(3000))();
